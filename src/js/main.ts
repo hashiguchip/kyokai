45bdb936d3b7c90deb4eb97bd52bd4541efd1b47
hashiguchip <hashiguchip@gmail.com>
2019-04-03 00:21:13 +0900

    [add] carousel by Swiper

--- src/js/main.ts
+++ src/js/main.ts
@@ -1,5 +1,20 @@
+import "swiper/dist/css/swiper.min.css";
 import "../css/main.scss";
+import Swiper from "swiper";
 
-export default () => {
-  console.log("test");
-};
+let mySwiper = new Swiper(".swiper-container", {
+  pagination: {
+    el: ".swiper-pagination"
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev"
+  },
+  scrollbar: {
+    el: ".swiper-scrollbar"
+  }
+});
+
+// export default () => {
+//   console.log("test");
+// };
