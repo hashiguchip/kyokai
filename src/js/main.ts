1a611186c3133f604428412fec98fad1cad08604
hashiguchip <hashiguchip@gmail.com>
2019-04-07 17:12:17 +0900

    [add] Parallax single page hero image

--- src/js/main.ts
+++ src/js/main.ts
@@ -1,7 +1,17 @@
 import "../css/main.scss";
+import Rellax from "rellax";
 import Swiper from "swiper";
 
-let mySwiper = new Swiper(".swiper-container", {
+const rellax = new Rellax(".js-hero", {
+  speed: -6,
+  center: false,
+  wrapper: null,
+  round: true,
+  vertical: true,
+  horizontal: false
+});
+
+const mySwiper = new Swiper(".swiper-container", {
   pagination: {
     el: ".swiper-pagination"
   },
